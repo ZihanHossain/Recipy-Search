2c37cb06312ebaecea1ee70b83afdbea459ccc5f
ZihanHossain <zihanhossain02@gmail.com>
2020-07-19 00:30:44 +0600

    Updates

---
+++ precache-manifest.05d2605c56a374c7038d8f67bd65d49e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc61f8a37e7e841331fb0ee41a31346",
+    "url": "/Recipy-Search/index.html"
+  },
+  {
+    "revision": "be80225b98ac53140f87",
+    "url": "/Recipy-Search/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "118d881c2751223e4d6b",
+    "url": "/Recipy-Search/static/css/main.b55889fd.chunk.css"
+  },
+  {
+    "revision": "be80225b98ac53140f87",
+    "url": "/Recipy-Search/static/js/2.b01625c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipy-Search/static/js/2.b01625c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "118d881c2751223e4d6b",
+    "url": "/Recipy-Search/static/js/main.0e2b0e0b.chunk.js"
+  },
+  {
+    "revision": "2afed491f43801cdec50",
+    "url": "/Recipy-Search/static/js/runtime-main.fa397084.js"
+  }
+]);
